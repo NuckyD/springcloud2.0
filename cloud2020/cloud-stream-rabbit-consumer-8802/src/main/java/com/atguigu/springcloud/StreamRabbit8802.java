7fcd5a7c745d83859fbb9040887f01ae3ae0f688
Nucky <411853404@qq.com>
2020-10-26 18:01:46 +0800

    Spring cloud Stream

---
+++ cloud2020/cloud-stream-rabbit-consumer-8802/src/main/java/com/atguigu/springcloud/StreamRabbit8802.java
@@ -0,0 +1,11 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamRabbit8802 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamRabbit8802.class, args);
+    }
+}
---
+++ cloud2020/cloud-stream-rabbit-provider-8801/src/main/java/com/atguigu/springcloud/service/Impl/MessageServiceImpl.java
@@ -0,0 +1,25 @@
+package com.atguigu.springcloud.service.Impl;
+
+import com.atguigu.springcloud.service.IMessageService;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.integration.support.MessageBuilder;
+import org.springframework.messaging.MessageChannel;
+
+import javax.annotation.Resource;
+import java.util.UUID;
+
+@EnableBinding(Source.class)
+public class MessageServiceImpl implements IMessageService {
+
+    @Resource
+    private MessageChannel output;
+
+    @Override
+    public String send() {
+        String serial = UUID.randomUUID().toString();
+        output.send(MessageBuilder.withPayload(serial).build());
+        System.out.println("*****serial: "+serial);
+        return serial;
+    }
+}
